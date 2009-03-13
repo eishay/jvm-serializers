6727dd6f331086ecaef211d4b9718e1f5ce54060
cpettitt <cpettitt@1a700704-b3b4-11dd-8d2d-9519526c4844>
2009-03-13 19:13:51 +0000

    Modify run-benchmark.sh to be compatible with the Java 1.5 JVM.
    git-svn-id: https://thrift-protobuf-compare.googlecode.com/svn/trunk@18 1a700704-b3b4-11dd-8d2d-9519526c4844

--- tpc/run-benchmark.sh
+++ tpc/run-benchmark.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 
-# note: need JDK 1.6 that support wildcard for classpath
-
-java -Xmx64m -cp build:lib/\* serializers.BenchmarkRunner
+java -Xmx64m -cp build:$(find lib -name "*.jar" | tr '\n' ':') serializers.BenchmarkRunner
