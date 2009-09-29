79c1fa1fbafdaefd27675bf997bbef2c04a0c19c
nathan.sweet <nathan.sweet@1a700704-b3b4-11dd-8d2d-9519526c4844>
2009-09-29 04:09:13 +0000

    Issue 9: ProtobufSerializer skips optional fields
    git-svn-id: https://thrift-protobuf-compare.googlecode.com/svn/trunk@83 1a700704-b3b4-11dd-8d2d-9519526c4844

--- tpc/src/serializers/ProtobufSerializer.java
+++ tpc/src/serializers/ProtobufSerializer.java
@@ -22,19 +22,44 @@ public class ProtobufSerializer implements ObjectSerializer<MediaContent>
         return content.toByteArray();
     }
 
-  public MediaContent create()
-  {
-    MediaContent content = MediaContent.newBuilder().
-    setMedia(
-      Media.newBuilder().setUri("http://javaone.com/keynote.mpg").setFormat("video/mpg4").setTitle("Javaone Keynote").setDuration(1234567).
-        setBitrate(123).addPerson("Bill Gates").addPerson("Steve Jobs").setPlayer(Player.JAVA).build()).
-    addImage(
-      Image.newBuilder().setUri("http://javaone.com/keynote_large.jpg").setSize(Size.LARGE).setTitle("Javaone Keynote").build()).
-    addImage(
-      Image.newBuilder().setUri("http://javaone.com/keynote_thumbnail.jpg").setSize(Size.SMALL).setTitle("Javaone Keynote").build()).
-    build();
-    return content;
-  }
+    public MediaContent create()
+    {
+    	MediaContent contentProto = MediaContent
+    	.newBuilder().setMedia(
+    		Media.newBuilder()
+    			.clearCopyright()
+    			.setFormat("video/mpg4")
+    			.setPlayer(Player.JAVA)
+    			.setTitle("Javaone Keynote")
+    			.setUri("http://javaone.com/keynote.mpg")
+    			.setDuration(1234567)
+    			.setSize(123)
+    			.setHeight(0)
+    			.setWidth(0)
+    			.setBitrate(123)
+    			.addPerson("Bill Gates")
+    			.addPerson("Steve Jobs")
+    			.build()
+    		).addImage(
+    			Image.newBuilder()
+    			.setHeight(0)
+    			.setTitle("Javaone Keynote")
+    			.setUri("http://javaone.com/keynote_large.jpg")
+    			.setWidth(0)
+    			.setSize(Size.LARGE)
+    			.build()
+    		).addImage(
+    			Image.newBuilder()
+    			.setHeight(0)
+    			.setTitle("Javaone Keynote")
+    			.setUri("http://javaone.com/keynote_thumbnail.jpg")
+    			.setWidth(0)
+    			.setSize(Size.SMALL)
+    			.build()
+    		)
+    		.build();
+    	return contentProto;
+    }
 
   public String getName ()
   {
