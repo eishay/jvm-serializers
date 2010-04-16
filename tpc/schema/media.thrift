4439d4307e2657728bb4e88e1c36d1ebf1b00a42
kannan@cakoose.com <kannan@cakoose.com@1a700704-b3b4-11dd-8d2d-9519526c4844>
2010-04-16 08:08:05 +0000

    In Thrift/Protobuf schemas, make required fields 'required' to try and make the test fair.
    git-svn-id: https://thrift-protobuf-compare.googlecode.com/svn/branches/kannan@158 1a700704-b3b4-11dd-8d2d-9519526c4844

--- tpc/schema/media.thrift
+++ tpc/schema/media.thrift
@@ -4,8 +4,8 @@ typedef i32 int
 typedef i64 long
 
 enum Size {
-  SMALL = 1,
-  LARGE = 2,
+  SMALL = 0,
+  LARGE = 1,
 }
 
 enum Player {
@@ -19,26 +19,26 @@ enum Player {
 struct Image {
   1: string uri,              //url to the images
   2: optional string title,  
-  3: optional int width,
-  4: optional int height,
-  5: optional Size size,
+  3: required int width,
+  4: required int height,
+  5: required Size size,
 }
 
 struct Media {
   1: string uri,             //url to the thumbnail
   2: optional string title,
-  3: optional int width,
-  4: optional int height,
-  5: optional string format,
-  6: optional long duration,
-  7: optional long size,
+  3: required int width,
+  4: required int height,
+  5: required string format,
+  6: required long duration,
+  7: required long size,
   8: optional int bitrate,
-  9: optional list<string> person,
-  10: optional Player player,
+  9: required list<string> person,
+  10: required Player player,
   11: optional string copyright,
 }
 
 struct MediaContent {
-  1: optional list<Image> image,
-  2: optional Media media,
+  1: required list<Image> image,
+  2: required Media media,
 }
