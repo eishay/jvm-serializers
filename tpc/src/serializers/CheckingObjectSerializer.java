02614a406ca1c7de822a27dd08470551736608bf
kannan@cakoose.com <kannan@cakoose.com@1a700704-b3b4-11dd-8d2d-9519526c4844>
2010-04-21 00:25:40 +0000

    Cleanup: Deleted old serializers that have been moved over to the new framework.
    git-svn-id: https://thrift-protobuf-compare.googlecode.com/svn/branches/kannan@172 1a700704-b3b4-11dd-8d2d-9519526c4844

--- tpc/src/serializers/CheckingObjectSerializer.java
+++
@@ -1,6 +0,0 @@
-package serializers;
-
-public interface CheckingObjectSerializer<T> extends ObjectSerializer<T> {
-    public void checkAllFields(T obj);
-    public void checkMediaField(T obj);
-}
