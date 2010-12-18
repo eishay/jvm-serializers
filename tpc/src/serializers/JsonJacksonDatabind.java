fabf406c232ea2f197ed3f9edc9a542b2470484e
Tatu Saloranta <tsaloranta@gmail.com>
2010-12-17 23:51:01 -0800

    Minor updates; update latest Jackson build, ensure correct root type for data binder

--- tpc/src/serializers/JsonJacksonDatabind.java
+++ tpc/src/serializers/JsonJacksonDatabind.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 
 import data.media.MediaContent;
 
-import org.codehaus.jackson.map.ObjectMapper;
+import org.codehaus.jackson.map.*;
+import org.codehaus.jackson.map.type.TypeFactory;
+import org.codehaus.jackson.type.JavaType;
 
 /**
  * This serializer uses Jackson in full automated data binding mode, which
@@ -16,6 +18,7 @@ public class JsonJacksonDatabind
 	public static void register(TestGroups groups)
 	{
 	        ObjectMapper mapper = new ObjectMapper();
+	        // note: could also force static typing; left out to keep defaults
 		groups.media.add(JavaBuiltIn.MediaTransformer,
 		        new GenericSerializer<MediaContent>("json/jackson-databind", mapper, MediaContent.class));
 	}
@@ -28,12 +31,13 @@ public class JsonJacksonDatabind
 	        private final String name;
 	        private final ObjectMapper mapper;
 
-	        private final Class<T> clazz;
+	        private final JavaType type;
+
 		public GenericSerializer(String name, ObjectMapper mapper, Class<T> clazz)
 		{
 		    this.name = name;
 		    this.mapper = mapper;
-		    this.clazz = clazz;
+		    this.type = TypeFactory.type(clazz);
 		}
 
 		public String getName() { return name; }
@@ -43,9 +47,10 @@ public class JsonJacksonDatabind
 			return mapper.writeValueAsBytes(data);
 		}
 
-		public T deserialize(byte[] array) throws Exception
-		{
-			return mapper.readValue(array, 0, array.length, clazz);
+		@SuppressWarnings("unchecked")
+                public T deserialize(byte[] array) throws Exception
+                {
+		    return (T) mapper.readValue(array, 0, array.length, type);
 		}
 	};
 }
