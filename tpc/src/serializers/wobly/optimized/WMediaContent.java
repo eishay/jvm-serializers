faacb13139c62272e031892916617f9e1d0095ad
ikabiljo <ikabiljo@gmail.com>
2011-08-06 14:56:53 +0100

    removed comments

--- tpc/src/serializers/wobly/optimized/WMediaContent.java
+++ tpc/src/serializers/wobly/optimized/WMediaContent.java
@@ -1,21 +1,3 @@
-/*******************************************************************************
- * Wobly - Wowd's byte-level serialization protocol
- * Copyright 2008-2011 Wowd Inc. All rights reserved.
- * http://code.google.com/p/wobly/
- *
- * Wobly is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation, either version 3
- * of the License, or (at your option) any later version.
- *
- * Wobly is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with Wobly. If not, see <http://www.gnu.org/licenses/>.
- *******************************************************************************/
 package serializers.wobly.optimized;
 
 import java.util.List;
