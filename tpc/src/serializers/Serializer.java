96d01ff57c20fb7e43ecd448c40a34a7dc305ecc
asdad <asdad>
2012-04-27 15:21:38 -0700

    Changed default BUFFER_SIZE to 1024 because 512 was too small for media.1.cks for 7 of the serializers.

--- tpc/src/serializers/Serializer.java
+++ tpc/src/serializers/Serializer.java
@@ -5,11 +5,11 @@ import java.io.*;
 public abstract class Serializer<S>
 {
     /**
-     * Buffer size for serializers.  Defaults to 512 and can be changed 
+     * Buffer size for serializers.  Defaults to 1024 and can be changed 
      * via system properties.  Minimum set to 256.
      */
     public static final int BUFFER_SIZE = Math.max(
-            Integer.getInteger("buffer_size", 512), 256);
+            Integer.getInteger("buffer_size", 1024), 256);
     
 	public abstract S deserialize(byte[] array) throws Exception;
 	public abstract byte[] serialize(S content) throws Exception;
