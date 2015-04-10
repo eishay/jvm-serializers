7b7fe9c2c74a5a7b15bcc5673461fca4f708788b
Bikram Bhusan Sinha <bsinha2@sapient.com>
2015-04-10 15:45:22 +0530

    Commit for testing

--- protowar/src/org/protowar/ProtoWar.java
+++ protowar/src/org/protowar/ProtoWar.java
@@ -13,11 +13,11 @@ import javax.servlet.http.HttpServletResponse;
 public class ProtoWar extends HttpServlet
 {
   private static final long serialVersionUID = 1L;
-  
-  private ProtoServerHandler _handler = new ProtoServerHandler(); 
+
+  private ProtoServerHandler _handler = new ProtoServerHandler();
 
   /**
-  * Default constructor. 
+  * Default Constructor.
   */
   public ProtoWar ()
   {
