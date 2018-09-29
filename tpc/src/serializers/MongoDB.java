288fde3609da09c4a3c2f2d7b69058860dd86e7f
Pascal S. de Kloe <pascal@quies.net>
2018-09-29 14:16:04 +0200

    Upgrade MongoDB BSON to 2.7.1.

--- tpc/src/serializers/MongoDB.java
+++ tpc/src/serializers/MongoDB.java
@@ -8,6 +8,7 @@ import java.util.List;
 import org.bson.BSONDecoder;
 import org.bson.BSONEncoder;
 import org.bson.BSONObject;
+import org.bson.BasicBSONDecoder;
 import org.bson.BasicBSONObject;
 import org.bson.types.BasicBSONList;
 
@@ -98,7 +99,7 @@ public class MongoDB
 		{
 			MediaContent r = new MediaContent();
 			
-			BSONDecoder dec = new BSONDecoder();
+			BSONDecoder dec = new BasicBSONDecoder();
 			BSONObject o = dec.readObject(array);
 			for (String name : o.keySet())
 			{
